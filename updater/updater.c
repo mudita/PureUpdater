cb2294133c558d7662f720943117b5b3c356b650
Lucjan Bryndza <lucjan.bryndza@mudita.com>
2021-06-21 13:34:09 +0200

    test for rw

--- updater/updater.c
+++ updater/updater.c
@@ -2,13 +2,19 @@
 #include <hal/delay.h>
 #include <hal/emmc.h>
 #include <stdio.h>
+#include <drivers/sdmmc/fsl_mmc.h>
 
+unsigned char buffer[512];
 
 int main()
 {   // System initialize
     sysinit_setup();
     // Main loop
-    printf("Ta karta EMMC chyba dziala%p\n", emmc_card());
+    printf("Ta karta EMMC chyba dziala %p\n", emmc_card());
+
+    // Try to retrive and read sector
+    printf("Read status %li\n",MMC_ReadBlocks(emmc_card(),buffer,0,1));
+    
     for(;;) {
         printf("test lepszego sprzetu %lu\n", get_jiffiess());
         msleep(1000);
