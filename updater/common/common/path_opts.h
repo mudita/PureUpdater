dc8b7c8992e86e0dfdef8ed0c384200c5cdf616f
Lucjan Bryndza <lucjan.bryndza@mudita.com>
2021-07-20 13:35:58 +0200

    [CP-287] Program SRK keys into the efuses from the file (#16)
    * [CP-287] Implements key update procedure
    
    Implements key update procedure in the check
    for the checksum if match
    
    * [CP-287] Fix validate keys file
    
    Fix validate keys checksum file
    
    * [CP-287] SRK keys program in efuses
    
    Add support in the updater to program
    SRK keys in the eFuses

--- updater/common/common/path_opts.h
+++ updater/common/common/path_opts.h
@@ -5,14 +5,14 @@ extern "C"
 {
 #endif
 
-
-/// remove duplicated slashes from path
-void path_remove_dup_slash(char *from);
-/// remove ./
-void path_remove_cwd(char *from);
-/// remove dup from path and entry /
-char *path_sanitize(char *from, char *path);
-void path_remove_trailing_slash(char* out);
+    /// remove duplicated slashes from path
+    void path_remove_dup_slash(char *from);
+    /// remove ./
+    void path_remove_cwd(char *from);
+    /// remove dup from path and entry /
+    char *path_sanitize(char *from, char *path);
+    void path_remove_trailing_slash(char *out);
+    const char *path_basename_const(const char *path);
 
 #ifdef __cplusplus
 }
