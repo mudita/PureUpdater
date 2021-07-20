e0652d71602ee850dab69841cf99248bbebff7cc
Lucjan Bryndza <lucjan.bryndza@mudita.com>
2021-07-20 15:50:18 +0200

    [EGD-391] Improve speed of the ecoboot update
    Improve the speed of the ecoboot update procesure
    Switch to use logger instead of simple printf
    
    Signed-off-by: Lucjan Bryndza <lucjan.bryndza@mudita.com>

--- updater/procedure/package_update/priv_update_ecoboot.c
+++ updater/procedure/package_update/priv_update_ecoboot.c
@@ -9,6 +9,8 @@
 
 //! Ecooboot partition handle
 #define ECO_PARTITION blk_disk_handle(blkdev_emmc_boot1, 0)
+// Number of sectors per transfer
+#define SECTORS_PER_TRANSFER 32
 
 //! For cleanup dynamic resources
 static void free_clean_up(uint8_t **ptr)
@@ -24,40 +26,71 @@ static void file_clean_up(FILE **fil)
         fclose(*fil);
     }
 }
+//! Error related translator
+static const char *strerror_pgm_keys(int err)
+{
+    switch ((enum ecoboot_update_errors)err)
+    {
+    case error_eco_update_ok:
+        return "Ecoboot update OK";
+    case error_eco_vfs:
+        return "VFS error";
+    case error_eco_blkdev:
+        return "Block device error";
+    case error_eco_system:
+        return "System error";
+    case error_eco_update_no_reg_file:
+        return "It is not a regular file";
+    case error_eco_too_small:
+        return "Ecoboot file to small";
+    case error_eco_verify:
+        return "Ecoboot verifcation error";
+    default:
+        return "";
+    }
+}
+
+//! Extended error
+static const char *strerror_ext_pgm_keys(int err, int err_ext)
+{
+    return (err >= error_eco_vfs && err <= error_eco_system) ? (strerror(-err_ext)) : ("");
+}
 
 /** Open ecoboot file in read only mode
  * and verify ecoboot header
  * @param[in] path Path to the ecoboot image
  * @param[out] file STDIO file handle
+ * @param[in] trace Log trace object
  * @return if positive image size otherwise error
  */
-static int open_ecoboot_file(const char *path, FILE **file)
+static int open_ecoboot_file(const char *path, FILE **file, trace_t *trace)
 {
     const off_t minimal_size = 64 * 1024;
     struct stat st;
     if (!file)
     {
+        printf("%s: Filename not provided\n", __PRETTY_FUNCTION__);
         return -EINVAL;
     }
     if (stat(path, &st))
     {
-        printf("%s: Unable to stat errno %i\n", __PRETTY_FUNCTION__, errno);
+        trace_write(trace, error_eco_vfs, -errno);
         return -errno;
     }
     if (!S_ISREG(st.st_mode))
     {
-        printf("%s: Is not regular file\n", __PRETTY_FUNCTION__);
+        trace_write(trace, error_eco_update_no_reg_file, 0);
         return -ENOEXEC;
     }
     if (st.st_size < minimal_size)
     {
-        printf("%s: Bootloader size is too small\n", __PRETTY_FUNCTION__);
+        trace_write(trace, error_eco_too_small, 0);
         return -ENOEXEC;
     }
     *file = fopen(path, "r");
     if (!*file)
     {
-        printf("%s: Unable to open file %i\n", __PRETTY_FUNCTION__, errno);
+        trace_write(trace, error_eco_vfs, -errno);
         return -errno;
     }
     return st.st_size;
@@ -65,32 +98,33 @@ static int open_ecoboot_file(const char *path, FILE **file)
 
 /** Flash the ecoboot device
  * @param[in] path Ecoboot path
+ * @param[in] trace Log trace object
  * @return error code
  */
-static int flash_ecoboot(const char *path)
+static int flash_ecoboot(const char *path, trace_t *trace)
 {
     blk_dev_info_t blk;
     int err, fil_size;
     FILE *fil __attribute__((__cleanup__(file_clean_up))) = NULL;
     if ((err = blk_info(ECO_PARTITION, &blk)))
     {
-        printf("%s: Unable to get part block %i\n", __PRETTY_FUNCTION__, err);
+        trace_write(trace, error_eco_blkdev, err);
         return err;
     }
-    uint8_t *sect_buf __attribute__((__cleanup__(free_clean_up))) = malloc(blk.sector_size);
+    uint8_t *sect_buf __attribute__((__cleanup__(free_clean_up))) = malloc(blk.sector_size * SECTORS_PER_TRANSFER);
     if (!sect_buf)
     {
-        printf("%s: No memory\n", __PRETTY_FUNCTION__);
-        return -ENOMEM;
+        trace_write(trace, error_eco_system, -errno);
+        return -errno;
     }
 
-    if ((fil_size = open_ecoboot_file(path, &fil)) <= 0)
+    if ((fil_size = open_ecoboot_file(path, &fil, trace)) <= 0)
     {
         return fil_size;
     }
     for (lba_t lba = 0, sz = fil_size; !feof(fil) && !ferror(fil);)
     {
-        if (sz < blk.sector_size)
+        if (sz < blk.sector_size * SECTORS_PER_TRANSFER)
         {
             memset(sect_buf, 0, blk.sector_size);
             if (!fread(sect_buf, sz, 1, fil))
@@ -101,11 +135,12 @@ static int flash_ecoboot(const char *path)
         }
         else
         {
-            if (!fread(sect_buf, blk.sector_size, 1, fil))
+            const size_t nread = fread(sect_buf, blk.sector_size, SECTORS_PER_TRANSFER, fil);
+            if (nread == 0)
             {
                 if (!ferror(fil))
                 {
-                    sz -= blk.sector_size;
+                    sz -= blk.sector_size * nread;
                     continue;
                 }
                 else
@@ -113,15 +148,18 @@ static int flash_ecoboot(const char *path)
                     break;
                 }
             }
-            sz -= blk.sector_size;
+            sz -= blk.sector_size * nread;
         }
-        if ((err = blk_write(ECO_PARTITION, lba++, 1, sect_buf)))
+        if ((err = blk_write(ECO_PARTITION, lba, SECTORS_PER_TRANSFER, sect_buf)))
         {
+            trace_write(trace, error_eco_blkdev, err);
             return err;
         }
+        lba += SECTORS_PER_TRANSFER;
     }
     if (ferror(fil))
     {
+        trace_write(trace, error_eco_vfs, -errno);
         return -errno;
     }
     else
@@ -132,42 +170,45 @@ static int flash_ecoboot(const char *path)
 
 /** Flash the ecoboot device
  * @param[in] path Ecoboot path
+ * @param[in] trace Log trace object
  * @return error code
  */
-static int verify_ecoboot(const char *path)
+static int verify_ecoboot(const char *path, trace_t *trace)
 {
     blk_dev_info_t blk;
     int err, fil_size;
     FILE *fil __attribute__((__cleanup__(file_clean_up))) = NULL;
     if ((err = blk_info(ECO_PARTITION, &blk)))
     {
-        printf("%s: Unable to get part block %i\n", __PRETTY_FUNCTION__, err);
+        trace_write(trace, error_eco_blkdev, err);
         return err;
     }
-    uint8_t *buf1 __attribute__((__cleanup__(free_clean_up))) = malloc(blk.sector_size);
+    uint8_t *buf1 __attribute__((__cleanup__(free_clean_up))) = malloc(blk.sector_size * SECTORS_PER_TRANSFER);
     if (!buf1)
     {
-        printf("%s: No memory\n", __PRETTY_FUNCTION__);
-        return -ENOMEM;
+        trace_write(trace, error_eco_system, -errno);
+        return -errno;
     }
-    uint8_t *buf2 __attribute__((__cleanup__(free_clean_up))) = malloc(blk.sector_size);
+    uint8_t *buf2 __attribute__((__cleanup__(free_clean_up))) = malloc(blk.sector_size * SECTORS_PER_TRANSFER);
     if (!buf2)
     {
-        printf("%s: No memory\n", __PRETTY_FUNCTION__);
-        return -ENOMEM;
+        trace_write(trace, error_eco_system, -errno);
+        return -errno;
     }
-    if ((fil_size = open_ecoboot_file(path, &fil)) <= 0)
+    if ((fil_size = open_ecoboot_file(path, &fil, trace)) <= 0)
     {
         return fil_size;
     }
     for (lba_t lba = 0, sz = fil_size; !feof(fil) && !ferror(fil);)
     {
-        if ((err = blk_read(ECO_PARTITION, lba++, 1, buf1)))
+        if ((err = blk_read(ECO_PARTITION, lba, SECTORS_PER_TRANSFER, buf1)))
         {
+            trace_write(trace, error_eco_blkdev, err);
             return err;
         }
+        lba += SECTORS_PER_TRANSFER;
 
-        if (sz < blk.sector_size)
+        if (sz < blk.sector_size * SECTORS_PER_TRANSFER)
         {
             memset(buf2, 0, blk.sector_size);
             if (!fread(buf2, sz, 1, fil))
@@ -178,11 +219,12 @@ static int verify_ecoboot(const char *path)
         }
         else
         {
-            if (!fread(buf2, blk.sector_size, 1, fil))
+            const size_t nread = fread(buf2, blk.sector_size, SECTORS_PER_TRANSFER, fil);
+            if (nread == 0)
             {
                 if (!ferror(fil))
                 {
-                    sz -= blk.sector_size;
+                    sz -= blk.sector_size * nread;
                     continue;
                 }
                 else
@@ -190,16 +232,17 @@ static int verify_ecoboot(const char *path)
                     break;
                 }
             }
-            sz -= blk.sector_size;
+            sz -= blk.sector_size * nread;
         }
-        if (memcmp(buf1, buf2, blk.sector_size) != 0)
+        if (memcmp(buf1, buf2, blk.sector_size * SECTORS_PER_TRANSFER) != 0)
         {
-            printf("%s: Ecoboot verication failed\n", __PRETTY_FUNCTION__);
-            return -EIO;
+            trace_write(trace, error_eco_verify, 0);
+            return error_eco_verify;
         }
     }
     if (ferror(fil))
     {
+        trace_write(trace, error_eco_vfs, -errno);
         return -errno;
     }
     else
@@ -209,17 +252,18 @@ static int verify_ecoboot(const char *path)
 }
 
 // Update the ecoboot
-int ecoboot_update(const char *path)
+int ecoboot_update(const char *path, trace_list_t *tl)
 {
     int ret;
+    trace_t *trace = trace_append(__PRETTY_FUNCTION__, tl, strerror_pgm_keys, strerror_ext_pgm_keys);
     do
     {
-        ret = flash_ecoboot(path);
+        ret = flash_ecoboot(path, trace);
         if (ret)
         {
             break;
         }
-        ret = verify_ecoboot(path);
+        ret = verify_ecoboot(path, trace);
         if (ret)
         {
             break;
--- updater/procedure/package_update/priv_update_ecoboot.h
+++ updater/procedure/package_update/priv_update_ecoboot.h
@@ -1,7 +1,24 @@
 #pragma once
 
+#include <common/trace.h>
+
+//! Error codes
+enum ecoboot_update_errors
+{
+    error_eco_update_ok,
+    // With errno
+    error_eco_vfs,    //! Errno related to vfs
+    error_eco_blkdev, //! Errrno related to blkdev
+    error_eco_system, //! Generic system error
+    //Without errno
+    error_eco_update_no_reg_file, //! Not a regular file
+    error_eco_too_small,          //! Bootloader too small
+    error_eco_verify              //! Verification failed
+};
+
 /** Update the ecoboot from the seleted file
  * @param[in] path Path to ecoboot binary file
+ * @param[in] tl Logger object
  * @return 0 if success -errno on failure
  */
-int ecoboot_update(const char *path);
+int ecoboot_update(const char *path, trace_list_t *tl);
