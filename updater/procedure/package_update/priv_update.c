07baa1da5aeb6a27c84fdaa12d991673f4e831c7
Lucjan Bryndza <lucjan.bryndza@mudita.com>
2021-08-11 14:21:11 +0200

    Uboot update test

--- updater/procedure/package_update/priv_update.c
+++ updater/procedure/package_update/priv_update.c
@@ -10,7 +10,6 @@ bool is_os_file(const char *file)
 {
     const char *os_files[] = {
         "boot.bin",
-        "ecoboot.bin",
         "updater.bin",
         "version.json",
         ".boot.json",
@@ -40,14 +39,16 @@ static const char *unpack_strerror_ext(int err, int ext_err)
 
 bool unpack(struct update_handle_s *handle, trace_list_t *trace_list)
 {
-    bool ret       = true;
-    int result     = 0;
-    trace_t *t     = trace_append("unpack", trace_list, update_strerror, unpack_strerror_ext);
+    bool ret = true;
+    int result = 0;
+    trace_t *t = trace_append("unpack", trace_list, update_strerror, unpack_strerror_ext);
     trace_t *tar_t = trace_append("tar", trace_list, tar_strerror, tar_strerror_ext);
     struct tar_ctx ctx;
 
-    do {
-        if (0 != tar_init(&ctx, tar_t, handle->update_from, "r")) {
+    do
+    {
+        if (0 != tar_init(&ctx, tar_t, handle->update_from, "r"))
+        {
             trace_write(t, ErrorUnpack, 0);
             ret = false;
             break;
@@ -56,26 +57,32 @@ bool unpack(struct update_handle_s *handle, trace_list_t *trace_list)
         int lib_error = MTAR_ESUCCESS;
         mtar_header_t header;
 
-        while ((lib_error = (mtar_read_header(&ctx.tar, &header))) != MTAR_ENULLRECORD) {
+        while ((lib_error = (mtar_read_header(&ctx.tar, &header))) != MTAR_ENULLRECORD)
+        {
             lib_error = mtar_read_header(&ctx.tar, &header);
 
-            if (lib_error != 0 && lib_error != MTAR_ENULLRECORD) {
+            if (lib_error != 0 && lib_error != MTAR_ENULLRECORD)
+            {
                 break;
             }
 
             const char *to = handle->tmp_user;
-            if (is_os_file(header.name) || should_not_be_on_os_but_is(header.name)) {
+            if (is_os_file(header.name) || should_not_be_on_os_but_is(header.name))
+            {
                 to = handle->tmp_os;
             }
 
-            if (header.type == MTAR_TDIR) {
+            if (header.type == MTAR_TDIR)
+            {
                 result = un_tar_catalog(&ctx, &header, to);
             }
-            else if (header.type == MTAR_TREG) {
+            else if (header.type == MTAR_TREG)
+            {
                 result = un_tar_file(&ctx, &header, to);
             }
 
-            if (result != 0) {
+            if (result != 0)
+            {
                 trace_write(t, ErrorUnpack, 0);
                 ret = false;
                 break;
@@ -83,21 +90,22 @@ bool unpack(struct update_handle_s *handle, trace_list_t *trace_list)
 
             if (tar_next(&ctx))
             {
-                trace_write(t, ErrorUnpack,0);
+                trace_write(t, ErrorUnpack, 0);
                 ret = false;
                 break;
             }
         }
 
-        if (0 != lib_error && lib_error != MTAR_ENULLRECORD) {
+        if (0 != lib_error && lib_error != MTAR_ENULLRECORD)
+        {
             trace_write(t, ErrorUnpack, 0);
             ret = false;
             break;
         }
     } while (0);
 
-
-    if (tar_deinit(&ctx)) {
+    if (tar_deinit(&ctx))
+    {
         trace_write(t, ErrorUnpack, 0);
         ret = false;
     }
